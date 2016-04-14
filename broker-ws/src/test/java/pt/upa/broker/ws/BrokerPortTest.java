288c3851c139a973230011997e6024653ddd683c
claudiaamorim <ana-claudia1995@hotmail.com>
2016-04-14 11:29:07 +0100

    inicio do brokertest a compilar e a dar tudo bem

--- broker-ws/src/test/java/pt/upa/broker/ws/BrokerPortTest.java
+++ broker-ws/src/test/java/pt/upa/broker/ws/BrokerPortTest.java
@@ -1,7 +1,9 @@
-/*package pt.upa.broker.ws;
+package pt.upa.broker.ws;
 
 import org.junit.*;
 import static org.junit.Assert.*;
+
+
 
 /**
  *  Unit Test example
@@ -10,7 +12,7 @@ import static org.junit.Assert.*;
  *  If necessary, should invoke "mock" remote servers 
  */
 
-/*
+
 public class BrokerPortTest {
 
     // static members
@@ -33,45 +35,43 @@ public class BrokerPortTest {
 
     BrokerPort broker;
 
+    TransportView transportview;
+
     // initialization and clean-up for each test
 
     @Before
     public void setUp() {
 
-        broker = new BrokerPort;
-        broker.initServiceSearch();
+        broker = new BrokerPort();
+        broker.initHandlersSearch();
     }
 
     @After
     public void tearDown() {
-        brokerjob = null;
+        broker = null;
     }
 
 
     // tests
 
-    @Test(expected = UnknownLocationFault_Exception)
+    @Test(expected = UnknownLocationFault_Exception.class)
     public void testUnknownOrigin() throws Exception{
             broker.requestTransport("Barreiro", "Faro", 45);
 
     }
 
-    @Test(expected = UnknownLocationFault_Exception)
+    @Test(expected = UnknownLocationFault_Exception.class)
     public void testUnknownDestinity() throws Exception{
             broker.requestTransport("Vila Real", "Amadora", 45);
 
     }
 
-    @Test(expected = InvalidPriceFault_Exception)
+    @Test(expected = InvalidPriceFault_Exception.class)
     public void testNegativePrice() throws Exception{
-            brokerjob.requestTransport("Vila Real", "Portalegre", -45);
+            broker.requestTransport("Vila Real", "Portalegre", -45);
 
     }
 
-    @Test(expected = UnvailableTransportFault_Exception)
-    public void testUnknownDestinity() throws Exception{
-            brokerjob.requestTransport("Vila Real", "Amadora", 45);
+    
 
-    }
-
-}*/
+}
