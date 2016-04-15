ab45cb2d890bb5be852b6d6cc8d9ef2226b51ca3
claudiaamorim <ana-claudia1995@hotmail.com>
2016-04-15 22:06:53 +0100

    it broker test

--- broker-ws-cli/target/generated-sources/wsimport/pt/upa/broker/ws/BrokerService.java
+++ broker-ws-cli/target/generated-sources/wsimport/pt/upa/broker/ws/BrokerService.java
@@ -17,7 +17,7 @@ import javax.xml.ws.WebServiceFeature;
  * Generated source version: 2.2
  * 
  */
-@WebServiceClient(name = "BrokerService", targetNamespace = "http://ws.broker.upa.pt/", wsdlLocation = "file:/C:/Users/DanielaD/Desktop/A_53-project/broker-ws-cli/../broker-ws/src/main/resources/broker.1_0.wsdl")
+@WebServiceClient(name = "BrokerService", targetNamespace = "http://ws.broker.upa.pt/", wsdlLocation = "file:/Users/claudiaamorim/Desktop/SD/Projeto/A_53-project/broker-ws-cli/../broker-ws/src/main/resources/broker.1_0.wsdl")
 public class BrokerService
     extends Service
 {
@@ -30,7 +30,7 @@ public class BrokerService
         URL url = null;
         WebServiceException e = null;
         try {
-            url = new URL("file:/C:/Users/DanielaD/Desktop/A_53-project/broker-ws-cli/../broker-ws/src/main/resources/broker.1_0.wsdl");
+            url = new URL("file:/Users/claudiaamorim/Desktop/SD/Projeto/A_53-project/broker-ws-cli/../broker-ws/src/main/resources/broker.1_0.wsdl");
         } catch (MalformedURLException ex) {
             e = new WebServiceException(ex);
         }
