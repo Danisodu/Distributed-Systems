56623fff9ecdf218778c1a03e8952da5274b66ca
Daniela <danisodu@gmail.com>
2016-05-06 02:07:59 +0100

    just a commit

---
+++ ca-ws/src/main/java/pt/upa/ca/ws/Certificate.java
@@ -0,0 +1,53 @@
+package pt.upa.ca.ws;
+
+import pt.upa.ca.ws.CAPort;
+
+
+import java.util.*;
+import java.security.PublicKey;
+import org.joda.time.DateTime;
+
+
+public class Certificate{
+	
+ 	private String companyName;
+    private PublicKey pubkey;
+    private DateTime timeout;
+
+    
+    public Certificate(){}
+    
+    public Certificate(String companyName, PublicKey pubkey, DateTime timeout) {
+    	
+    	setCompanyName(companyName);
+    	setPubKey(pubkey);
+    	setTimeout(timeout);
+    	
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+	
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	
+	public PublicKey getPubKey() {
+		return pubkey;
+	}
+
+	public void setPubKey(PublicKey pubkey) {
+		this.pubkey=pubkey;
+	}
+	
+	
+	public DateTime getTimeout() {
+		return timeout;
+	}
+	
+	public void setTimeout(DateTime timeout) {
+		this.timeout = timeout;
+	}
+	
+}
